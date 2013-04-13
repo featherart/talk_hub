eeee2782f3fd654c4cf31ff3507259c220280720
Feather <lisafeather@gmail.com>
2013-04-13 10:27:51 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TalkHub::Application.config.secret_token = 'b358548cbd04e2d4ba9af5f7514a414220daec3dd508cd9cc21e9d1b49dc5bc0608d6e479b8c2653d07b3ea5c13a77e2137da92fe71ad0241cc59d13fdb6f309'
